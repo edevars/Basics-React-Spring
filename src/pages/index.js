40254e6a40f4732d3cec21933fd51a1dd38fd991
Enrique Devars <e.devars.dev@gmail.com>
2020-05-03 21:42:33 -0500

    :recycle: Migrating from CRA to GatsbyJS

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { App } from "../Components/App";
+import "materialize-css/dist/css/materialize.css";
+import "materialize-css/dist/js/materialize";
+import "../assets/styles/main.css";
+
+const Home = () => {
+  return <App />;
+};
+
+export default Home;
