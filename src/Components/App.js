3fd9205d227e6125b7507ab15d25d5757943cd1f
Enrique Devars <e.devars.dev@gmail.com>
2020-05-03 19:27:52 -0500

    :sparkles: Adding Homepage
    Creating preliminar Homapage

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Header } from "./Header";
 import { IconContext } from "react-icons";
+import {HomePage} from "./HomePage";
+
 
 export const App = () => {
   return (
     <IconContext.Provider value={{ className: "react-icons" }}>
       <Header />
+      <HomePage />
     </IconContext.Provider>
   );
 };
---
+++ src/Components/HomePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Search } from "./Search";
+
+export const HomePage = () => {
+  return (
+    <main className="container">
+      <section className="section">
+        <Search />
+      </section>
+      <h3 className="example-title">Examples</h3>
+      <div className="divider" />
+      <section className="section">
+        <div className="row">
+        
+        </div>
+      </section>
+    </main>
+  );
+};
