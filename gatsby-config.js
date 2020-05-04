b8e124c2377c817442aaf005b685344308b05c5c
Enrique Devars <e.devars.dev@gmail.com>
2020-05-04 10:31:14 -0500

    :zap: Gatsby config for react-helmet

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `React Spring Basics`,
+    description: `A variety of basic animations to have fun with React`,
+  },
+  plugins: [`gatsby-plugin-react-helmet`],
+};
