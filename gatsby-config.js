0e38f49536cce4dda17e35d0ae64de504d906130
Enrique Devars <e.devars.dev@gmail.com>
2020-05-04 21:58:38 -0500

    :sparkles: Adding icon and manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,19 @@ module.exports = {
     title: `React Spring Basics`,
     description: `A variety of basic animations to have fun with React`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Basics React Spring`,
+        short_name: `Basics RS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#b100c8`,
+        display: `standalone`,
+        icon: `src/images/icon.png`
+      },
+    },
+  ],
 };
