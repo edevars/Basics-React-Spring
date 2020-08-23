b43ae55d4e396d755a172a74cd953d8c13f5193d
edevars <e.devars.dev@gmail.com>
2020-08-23 18:36:54 -0500

    :sparkles: Adding examples page

---
+++ src/pages/examples.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Layout } from '../Components/Layout';
+
+export default () => (
+    <Layout title="Examples" disableHeader>
+        <main className="container">
+            <section className="section">
+                <h1>Examples React Spring!</h1>
+            </section>
+        </main>
+    </Layout>
+)
